fb6e0e9e9cffa86fca53a5661b44415150bfe4ad
Luis De Gouveia <degouveiasousa@gmail.com>
2021-04-26 14:41:48 +0200

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 import Cita from './components/Cita';
 
 function App() {
 
+  //citas en el local storage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+
+  if( !citasIniciales ) {
+    citasIniciales = [];
+  }
+
   //arreglo de citas
-  const [ citas, guardarCitas ] = useState([]);
+  const [ citas, guardarCitas ] = useState(citasIniciales);
+
+  //Use Effect para realizar ciertas operaciones cuando el state cambia
+  useEffect( () => {
+    let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+
+    if( citasIniciales ) {
+      localStorage.setItem('citas', JSON.stringify(citas))
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas] );
 
   //funcion que tome las citas actuales y agregue la nueva
   const crearCita = cita => {
